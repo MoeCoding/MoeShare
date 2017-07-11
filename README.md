7db7bf7ea803af5f784fb1aa87db68bf4d5fb8dd
Luncode <1370655052@qq.com>
2017-07-11 17:12:10 +0800

    上传

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# MoeShare
-MoeShare 分享图片贺卡接入微信分享给朋友和朋友圈的软件
+##MoeShare是什么?
+一个分享图片贺卡接入微信分享给朋友和朋友圈的软件
+
+##MoeShare有哪些功能？
+* 分享图片贺卡接入微信分享给朋友和朋友圈
+
+##效果图
+![MoeShare](img/1.png)![MoeShare](img/2.png)
+
+##有问题反馈
+在使用中有任何问题，欢迎反馈给我，可以用以下联系方式跟我交流
+* 邮件(a1370655052#gmail.com, 把#换成@)
+
+##微信开放平台
+* [开放平台地址](http://open.weixin.qq.com/) 
+
+##关于
+*作者:Luncode
+
+*该项目直接用Android Studio导入
---
+++ app/src/main/java/moeshare/luncode/com/moeshare/wxapi/WXEntryActivity.java
@@ -0,0 +1,41 @@
+package moeshare.luncode.com.moeshare.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+/**
+ * Created by Administrator on 2017/7/7.
+ */
+
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler{
+    private IWXAPI api;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        api = WXAPIFactory.createWXAPI(this,"填写微信申请的appid",false);
+        api.handleIntent(getIntent(),this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        switch (baseResp.errCode){
+            case BaseResp.ErrCode.ERR_OK:
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                break;
+        }
+    }
+}
